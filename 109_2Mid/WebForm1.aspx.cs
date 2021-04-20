1bb336a5ce0454c9f62eb213b885ddc147d26f8d
冠誼 李 <zerorsqq676729@gmail.com>
2021-04-20 14:05:08 +0800

    1

--- 109_2Mid/WebForm1.aspx.cs
+++ 109_2Mid/WebForm1.aspx.cs
@@ -5,10 +5,13 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace _109_2Mid {
-    public partial class Sample1 : System.Web.UI.Page {
-        protected void Page_Load(object sender, EventArgs e) {
-11111
+namespace _109_2Mid
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
